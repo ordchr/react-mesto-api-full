0743c1137a82aebab07ba0da3191633aa0ca97fc
Антон <mongoostic@yandex.ru>
2020-11-21 19:36:34 +0800

    Логирование

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,12 @@ require('dotenv').config();
 
 const bodyParser = require('body-parser');
 const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
 
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { login, createUser } = require('./controllers/users');
 
@@ -24,6 +26,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 const app = express();
 app.use(bodyParser.json());
+app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -49,6 +52,10 @@ app.use('/cards', cards);
 app.use((req, res) => {
   res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
+
+app.use(errorLogger);
+
+app.use(errors());
 app.use((err, req, res, next) => {
   if (err.message.includes('validation failed')) {
     res.status(400);
