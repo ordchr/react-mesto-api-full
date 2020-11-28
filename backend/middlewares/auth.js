821598953dc5e90c83e4f41bb6bd011d722211da
Антон <mongoostic@yandex.ru>
2020-11-28 20:09:42 +0800

    Рефакторинг

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET = '70dee671c0f7f50d9d9c8faff790f88b359e788b130f2d6d81ba2436e445c532' } = process.env;
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
